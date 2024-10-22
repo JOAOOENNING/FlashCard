e809dbd43918869d886696c494f7954bb3b1dbf5
JOAOOENNING <146237359+JOAOOENNING@users.noreply.github.com>
2024-10-22 09:57:30 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,33 @@ criaCartao(
     'É possível criar animais no Farming Simulator?',
     'Sim, os jogadores podem criar animais como vacas, ovelhas, porcos e galinhas, gerenciando sua alimentação e cuidados para maximizar a produção de leite, lã e carne.'
 )
+
+criaCartao(
+    'Simulação',
+    'Como posso aumentar minha renda no jogo?',
+    'Os jogadores podem aumentar sua renda cultivando e vendendo culturas, criando e vendendo animais, e explorando contratos de trabalho para outros fazendeiros.'
+)
+
+criaCartao(
+    'Simulação',
+    'O que são mods e como eles afetam o jogo?',
+    'Mods são modificações feitas por usuários que podem adicionar novos veículos, equipamentos, mapas e recursos ao jogo, ampliando a experiência e as possibilidades de gameplay.'
+)
+
+criaCartao(
+    'Simulação',
+    'Como funciona o sistema de estações no jogo?',
+    'Farming Simulator apresenta um ciclo de estações que afeta o crescimento das culturas, tornando algumas plantas disponíveis apenas em determinadas épocas do ano.'
+)
+
+criaCartao(
+    'Simulação',
+    'É possível jogar em modo multiplayer?',
+    'Sim, o jogo oferece um modo multiplayer onde você pode se juntar a amigos ou outros jogadores para gerenciar uma fazenda em conjunto ou competir entre si.'
+)
+
+criaCartao(
+    'Simulação',
+    'Quais são algumas das máquinas agrícolas mais comuns no jogo?',
+    'Algumas máquinas comuns incluem tratores, colheitadeiras, semeadoras e pulverizadores, cada uma com funções específicas para facilitar o trabalho na fazenda.'
+)
